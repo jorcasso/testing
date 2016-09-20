3c8ce9e1700122d5afd25be541771f1704ec50ec
Jorge Castellanos <jorcasso@gmail.com>
2016-09-20 16:41:58 +0200

    Add more sites

--- src/main/java/com/kii/test/util/Site.java
+++ src/main/java/com/kii/test/util/Site.java
@@ -1,5 +1,5 @@
 package com.kii.test.util;
 
 public enum Site {
-	DEV_JP, STG_JP, PROD_JP
+	DEV_JP, DEV_JP_01, DEV_JP_02, STG_JP, PROD_JP, DEV_JP_LOCAL, STG_JP_LOCAL, PROD_JP_LOCAL
 }
--- src/main/java/com/kii/test/util/SiteUtil.java
+++ src/main/java/com/kii/test/util/SiteUtil.java
@@ -7,12 +7,16 @@ import java.util.Properties;
 public class SiteUtil {
 
 	private static String UFE_DEV_JP;
+	private static String UFE_DEV_JP_01;
+	private static String UFE_DEV_JP_02;
 	private static String UFE_STG_JP;
 	private static String UFE_PROD_JP;
+	private static String UFE_LOCAL;
 
 	private static String THING_IF_DEV_JP;
 	private static String THING_IF_STG_JP;
 	private static String THING_IF_PROD_JP;
+	private static String THING_IF_LOCAL;
 
 	private static String APP_DEV_JP;
 	private static String APP_STG_JP;
@@ -33,11 +37,15 @@ public class SiteUtil {
 			stream.close();
 
 			UFE_DEV_JP = sites.getProperty("ufe-dev-jp");
+			UFE_DEV_JP_01 = sites.getProperty("ufe-dev-jp-01");
+			UFE_DEV_JP_02 = sites.getProperty("ufe-dev-jp-02");
 			UFE_STG_JP = sites.getProperty("ufe-stg-jp");
 			UFE_PROD_JP = sites.getProperty("ufe-prod-jp");
+			UFE_LOCAL = sites.getProperty("ufe-local");
 			THING_IF_DEV_JP = sites.getProperty("thing-if-dev-jp");
 			THING_IF_STG_JP = sites.getProperty("thing-if-stg-jp");
 			THING_IF_PROD_JP = sites.getProperty("thing-if-prod-jp");
+			THING_IF_LOCAL = sites.getProperty("thing-if-local");
 		} catch (IOException e) {
 			System.out.println("failed to read sites");
 			System.exit(1);
@@ -68,10 +76,18 @@ public class SiteUtil {
 		switch (site) {
 		case DEV_JP:
 			return UFE_DEV_JP + separator + path;
+		case DEV_JP_01:
+			return UFE_DEV_JP_01 + separator + path;
+		case DEV_JP_02:
+			return UFE_DEV_JP_02 + separator + path;
 		case STG_JP:
 			return UFE_STG_JP + separator + path;
 		case PROD_JP:
 			return UFE_PROD_JP + separator + path;
+		case DEV_JP_LOCAL:
+		case STG_JP_LOCAL:
+		case PROD_JP_LOCAL:
+			return UFE_LOCAL + separator + path;
 		default:
 			throw new RuntimeException("Unknown site " + site);
 		}
@@ -87,6 +103,10 @@ public class SiteUtil {
 			return THING_IF_STG_JP + separator + path;
 		case PROD_JP:
 			return THING_IF_PROD_JP + separator + path;
+		case DEV_JP_LOCAL:
+		case STG_JP_LOCAL:
+		case PROD_JP_LOCAL:
+			return THING_IF_LOCAL + separator + path;
 		default:
 			throw new RuntimeException("Unknown site " + site);
 		}
@@ -95,10 +115,15 @@ public class SiteUtil {
 	public static String getApp(Site site) {
 		switch (site) {
 		case DEV_JP:
+		case DEV_JP_01:
+		case DEV_JP_02:
+		case DEV_JP_LOCAL:
 			return APP_DEV_JP;
 		case STG_JP:
+		case STG_JP_LOCAL:
 			return APP_STG_JP;
 		case PROD_JP:
+		case PROD_JP_LOCAL:
 			return APP_PROD_JP;
 		default:
 			throw new RuntimeException("Unknown site " + site);
